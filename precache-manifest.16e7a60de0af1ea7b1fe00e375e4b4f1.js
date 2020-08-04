6a3fcdc229b3e9fd0182aff5cdac2df7a79556c2
Fauzan Linnas <fauzanlinnas@gmail.com>
2020-08-05 02:09:54 +0700

    Updates

---
+++ precache-manifest.16e7a60de0af1ea7b1fe00e375e4b4f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be4b016335073af6d345bbee1387e52d",
+    "url": "/react-contact/index.html"
+  },
+  {
+    "revision": "29f05c83aee681aa88bd",
+    "url": "/react-contact/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "a9869830e06fb9004ceb",
+    "url": "/react-contact/static/js/2.e6762a5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-contact/static/js/2.e6762a5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29f05c83aee681aa88bd",
+    "url": "/react-contact/static/js/main.7f536ab0.chunk.js"
+  },
+  {
+    "revision": "45a3467ce0dc6c704312",
+    "url": "/react-contact/static/js/runtime-main.76e64950.js"
+  }
+]);
